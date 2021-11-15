dbc074cedf5441bf3f510491166fa400950fe49a
ChristopherEeles <christopher.eeles@outlook.com>
2021-11-15 17:57:58 -0500

    Fix: vignettes failing to build on CRAN
    * Vignette builds were failing for CRAN on the R-devel version with the new gcc10 compiler
    * Also failing builds on fedora, I am assuming it is for the same reason

--- R/PredictionsDistributionPie.R
+++ R/PredictionsDistributionPie.R
@@ -10,28 +10,25 @@
 #################################################################################
 #################################################################################
 
-PredictionsDistributionPie<-function(InputMatrix,pdf_output,pdfheight,pdfwidth)
-{
+PredictionsDistributionPie <- function(InputMatrix, pdf_output, pdfheight, 
+    pdfwidth) {
   colorscheme<-c("#fccde5","#ccebc5","#80b1d3","#bc80bd","#fb8072")
   
-  if(is.logical(pdf_output))
-  {
-    if(pdf_output==TRUE)
-    {
-      if((is.numeric(pdfheight))&&(is.numeric(pdfwidth)))
-      {
-      pdf(file="MM2S_OverallPredictions_PieDist.pdf",height=pdfheight,width=pdfwidth)
-      pie(c(table(InputMatrix$MM2S_Subtype[,2])),main="Overall Predictions",labelcex = 0.6,col=colorscheme)
-      dev.off()
+  if(is.logical(pdf_output)) {
+    if(pdf_output==TRUE) {
+      if((is.numeric(pdfheight))&&(is.numeric(pdfwidth))) {
+        pdf(file="MM2S_OverallPredictions_PieDist.pdf", height=pdfheight, 
+          width=pdfwidth)
+        pie(c(table(InputMatrix$MM2S_Subtype[,2])), main="Overall Predictions",
+          col=colorscheme)
+        dev.off()
+      } else {
+        stop("PDF dimensions must be numeric")
       }
-      else
-      {message("PDF dimensions must be numeric")
-      stop()}
     }
+  } else {
+    stop("TRUE or FALSE needed for PDF output")
   }
-  else {
-    message("TRUE or FALSE needed for PDF output")
-    stop()}
-  
-  pie(c(table(InputMatrix$MM2S_Subtype[,2])),main="Overall Predictions",labelcex = 0.6,col=colorscheme)
+  pie(c(table(InputMatrix$MM2S_Subtype[,2])), main="Overall Predictions",
+    col=colorscheme)
 }
